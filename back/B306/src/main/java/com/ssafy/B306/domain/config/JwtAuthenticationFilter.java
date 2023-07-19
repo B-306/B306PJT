ca7353723f022191c6fcae64adbe59afdd44754d
송혁준 <thdgurws98@naver.com>
2023-07-19 23:31:35 +0900

    [feat] jwt token 시도 230719 2331

--- back/B306/src/main/java/com/ssafy/B306/domain/config/JwtAuthenticationFilter.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/config/JwtAuthenticationFilter.java
@@ -1,8 +1,25 @@
 package com.ssafy.B306.domain.config;
 
-import javax.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter implements Filter {
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
     }
 }
