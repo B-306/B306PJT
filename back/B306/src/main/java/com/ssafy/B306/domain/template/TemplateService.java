e6471c51cfbb4cc34fba20c2a7bc0493ac1e1929
송혁준 <thdgurws98@gmail.com>
2023-07-21 15:53:35 +0900

    [fix] Bean 순환 참조 수정 230721 1553

--- back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateService.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateService.java
@@ -9,6 +9,5 @@ import org.springframework.stereotype.Service;
 public class TemplateService {
 
     private final TemplateRepository templateRepository;
-    private final TemplateService templateService;
 
 }
--- back/B306/src/main/java/com/ssafy/B306/domain/user/UserRepository.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/user/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ssafy.B306.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserEmail(String userEmail);
-//    User findByUserId(String userEmail);
 
-    boolean existsByEmail(String email);
+    boolean existsByUserEmail(String userEmail);
 }
 
