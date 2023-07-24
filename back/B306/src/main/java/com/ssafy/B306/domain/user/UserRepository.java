2fc81dcdf35f22eaba793dc68569c85ca6592966
송혁준 <thdgurws98@gmail.com>
2023-07-24 14:22:37 +0900

    [feat] jwt 로그인 구현 230724 1422

--- back/B306/src/main/java/com/ssafy/B306/domain/user/UserRepository.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.B306.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
