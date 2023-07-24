a57d1236b3af291853f591fdb9be50d82d063287
fsddcx8624 <fsddcx8624@gmail.com>
2023-07-24 15:19:41 +0900

    [Feat]: 템플릿 등록 기능 및 조회 기능 구현 230724 1518 템플릿 등록, 템플릿 조회 기능 controller, service, saveDto파일 등 작업

--- back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateRepository.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateRepository.java
@@ -3,11 +3,16 @@ package com.ssafy.B306.domain.template;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface TemplateRepository extends JpaRepository<Template, Long> {
+public interface TemplateRepository extends JpaRepository<Template, Long>{
+    Optional<Template> findByTemplateId(Long templateId);
 
-    Template findByTemplateId(Long templateId);
+
+    List<Template> findAll();
+
+
 
 }
--- back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateService.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/template/TemplateService.java
@@ -1,8 +1,13 @@
 package com.ssafy.B306.domain.template;
 
 
+import com.ssafy.B306.domain.template.dto.TemplateRequestDto;
+import com.ssafy.B306.domain.template.dto.TemplateSaveDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,20 @@ public class TemplateService {
 
     private final TemplateRepository templateRepository;
 
+    public Template getTemplateAddress(TemplateRequestDto templateRequest){
+        return templateRepository.findByTemplateId(templateRequest.getTemplateId()).orElseThrow(() -> new RuntimeException("no template"));
+    }
+
+    public List<Template> getAllTemplate() {
+        List<Template> temList = new ArrayList<>();
+        temList = templateRepository.findAll();
+        return temList;
+    }
+
+
+
+    public void addTemplate(TemplateSaveDto templateSaveDto) {
+        Template newTemp = templateSaveDto.toEntity(templateSaveDto);
+        templateRepository.save(newTemp);
+    }
 }
