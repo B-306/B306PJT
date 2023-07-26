ddea62fcfc1b1118df8f7749bdb37bf141b282d7
송혁준 <thdgurws98@gmail.com>
2023-07-26 17:22:04 +0900

    [feat] refresh token 발행 API 개발 230726 1722

--- back/B306/src/main/java/com/ssafy/B306/domain/user/UserController.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/user/UserController.java
@@ -3,6 +3,7 @@ package com.ssafy.B306.domain.user;
 import com.ssafy.B306.domain.security.JwtToken;
 import com.ssafy.B306.domain.user.dto.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto userLoginRequest) {
+    public ResponseEntity<?> login(@RequestBody UserLoginRequestDto userLoginRequest) {
         try{
             JwtToken token = userService.login(userLoginRequest);
-            return new ResponseEntity<>(new UserLoginResponseDto(token), HttpStatus.OK);
+            HttpHeaders header = new HttpHeaders();
+            header.add("accessToken", token.getAccessToken());
+            header.add("refreshToken", token.getRefreshToken());
+
+            return new ResponseEntity<>(new UserLoginResponseDto(token), header, HttpStatus.OK);
+
+
+//            return new ResponseEntity<>(new UserLoginResponseDto(token), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -42,14 +50,15 @@ public class UserController {
     1. user table에 저장하는 방식
     2. refresh token table을 만들어 저장 (user_id, refresh token, delete_date)
      */
-//    @PostMapping("/refresh")
-//    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
-//        try {
-//            JwtToken token = userService.refreshToken(request);
-//            return new ResponseEntity<>(token, HttpStatus.OK);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
+        try {
+            // 여기는 access token만 있음
+            JwtToken token = userService.refreshToken(request);
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
