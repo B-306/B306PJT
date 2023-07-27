b451e452dac41fae9a28ead0131548a150701c49
hzim <hzim0422@gmail.com>
2023-07-27 11:15:18 +0900

    [feat]: 이메일 인증 API 개발 진행중 230727 1115

---
+++ back/B306/src/main/java/com/ssafy/B306/domain/mail/EmailAuthCustomRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.B306.domain.mail;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface EmailAuthCustomRepository {
+    Optional<EmailAuth> findValidAuthByEmail(String email, String authToken, LocalDateTime currentTime);
+}
--- back/B306/src/main/java/com/ssafy/B306/domain/mail/MailAuthCustomRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.B306.domain.mail;
-
-public interface MailAuthCustomRepository {
-    
-}
