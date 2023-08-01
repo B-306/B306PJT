c72512687baf6643de257250a75e62f6c7f29107
JNH <math_star@naver.com>
2023-08-01 13:44:57 +0900

    [refactor] Redux 상태 관리 보강 20230801 1344
    Redux로 유저 로그인 상태 관리 및 Redux-persist로 상태 유지하도록 코드 보강

--- front/src/index.js
+++ front/src/index.js
@@ -13,21 +13,30 @@
 // );
 
 
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; // 1. Provider를 import합니다.
+import store from './redux/config/store'; // Redux 스토어를 import합니다.
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/config/store';
 
 // 1. ReactDOM.render 대신 createRoot로 변경
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
   
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 reportWebVitals();
---
+++ front/src/redux/config/AuthMiddleware.js
@@ -0,0 +1,31 @@
+// authMiddleware.js
+import { setTokens, setUserData, clearAuthData } from "../modules/authSlice";
+
+export const checkLoginStatus = () => async (dispatch, getState) => {
+  const state = getState();
+  const accessToken = state.auth.accessToken;
+  const refreshToken= state.auth.refreshToken;
+  // const userEmail = state.auth.userEmail;
+  const userName = state.auth.userName;
+  // console.dir(state.auth);
+  if (!accessToken) {
+    // 로그인 상태가 아니므로 리덕스 스토어 상태를 초기화합니다.
+    console.log('1번 초기화')
+    dispatch(clearAuthData());
+  } else {
+    try {
+      // 서버에 토큰을 전송하여 유효성을 검사하는 대신, 여기서는 유효성 검사를 생략합니다.
+      // 유효성 검사 로직이 필요한 경우 해당 부분을 추가하시면 됩니다.
+      
+      // 토큰이 유효한 경우 유저 데이터를 리덕스 스토어에 저장합니다.
+      // dispatch(setUserData({ userEmail }));
+      // console.log('초기화 안함')
+      dispatch(setTokens({ accessToken: accessToken, refreshToken: refreshToken }));
+      dispatch(setUserData({ userName: userName })); // , userEmail: userEmail
+    } catch (error) {
+      // 토큰이 유효하지 않은 경우 로그인 상태를 초기화합니다.
+      console.log('2번 초기화')
+      dispatch(clearAuthData());
+    }
+  }
+};
