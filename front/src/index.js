1a668d487ae884a81f049f93f9ac461ae65638d3
주영인 <jyi8714@naver.com>
2023-08-01 09:21:22 +0900

    [Feat]: redux 사용을 위한 provider 기능 추가

--- front/src/index.js
+++ front/src/index.js
@@ -13,21 +13,26 @@
 // );
 
 
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; // 1. Provider를 import합니다.
+import store from './redux/config/store'; // Redux 스토어를 import합니다.
 
 // 1. ReactDOM.render 대신 createRoot로 변경
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
   
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
