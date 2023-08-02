7e5a9f5275bbde21ac848deac990b271528701f5
주영인 <jyi8714@naver.com>
2023-08-02 22:23:12 +0900

    [Feat] : store 기능 구현

--- front/src/redux/config/store.js
+++ front/src/redux/config/store.js
@@ -1,11 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import authReducer from '../modules/authSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+
+
+// config 작성
+const persistConfig = {
+  key: "root", // localStorage key 
+  storage, // localStorage
+  whitelist: ["auth"], // target (reducer name)
+}
+
+const reducers = combineReducers({
+  auth: authReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    // Add other reducers here if you have any
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+  // {
+  //   auth: authReducer,
+  //   // Add other reducers here if you have any
+  // },
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export const persistor = persistStore(store);
 
 export default store;
