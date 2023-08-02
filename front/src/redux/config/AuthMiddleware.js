bc924e10467e03360db0917693fe5ac30fca9e3d
주영인 <jyi8714@naver.com>
2023-08-02 22:22:34 +0900

    [Feat] : 미들웨어 구현

--- front/src/redux/config/AuthMiddleware.js
+++ front/src/redux/config/AuthMiddleware.js
@@ -5,9 +5,9 @@ export const checkLoginStatus = () => async (dispatch, getState) => {
   const state = getState();
   const accessToken = state.auth.accessToken;
   const refreshToken= state.auth.refreshToken;
-  const userEmail = state.auth.userEmail;
+  // const userEmail = state.auth.userEmail;
   const userName = state.auth.userName;
-  console.dir(state.auth);
+  // console.dir(state.auth);
   if (!accessToken) {
     // 로그인 상태가 아니므로 리덕스 스토어 상태를 초기화합니다.
     console.log('1번 초기화')
@@ -19,9 +19,9 @@ export const checkLoginStatus = () => async (dispatch, getState) => {
       
       // 토큰이 유효한 경우 유저 데이터를 리덕스 스토어에 저장합니다.
       // dispatch(setUserData({ userEmail }));
-      console.log('초기화 안함')
+      // console.log('초기화 안함')
       dispatch(setTokens({ accessToken: accessToken, refreshToken: refreshToken }));
-      dispatch(setUserData({ userName: userName, userEmail: userEmail }));
+      dispatch(setUserData({ userName: userName })); // , userEmail: userEmail
     } catch (error) {
       // 토큰이 유효하지 않은 경우 로그인 상태를 초기화합니다.
       console.log('2번 초기화')
