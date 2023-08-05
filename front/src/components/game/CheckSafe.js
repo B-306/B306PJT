184a1fe4f3cc9252cc0554c38f8ff39d86f27cce
JNH <math_star@naver.com>
2023-08-05 19:20:13 +0900

    [refactor] 채점 알고리즘 보강 20230805 1919
    이미지비트맵 형식의 데이터를 이미지데이터로 변경

--- front/src/components/game/CheckSafe.js
+++ front/src/components/game/CheckSafe.js
@@ -87,24 +87,26 @@ class Check extends Component {
             return null; // 세그멘테이션 결과가 없으면 아무것도 렌더링하지 않음
         }
 
-        // 이미지 데이터 뒤집기
-        const maskImageData = people[0].mask.mask;
-        console.log(maskImageData)
+        const maskImageBitmap = people[0].mask.mask;
+        const maskImageData = imageBitmapToImageData(maskImageBitmap);
         
-        // const flippedImageData = new ImageData(maskImageData.width, maskImageData.height);
-    
-        // for (let y = 0; y < maskImageData.height; y++) {
-        //     for (let x = 0; x < maskImageData.width; x++) {
-        //         const sourceIndex = (y * maskImageData.width + (maskImageData.width - 1 - x)) * 4;
-        //         const targetIndex = (y * maskImageData.width + x) * 4;
-    
-        //         flippedImageData.data[targetIndex] = maskImageData.data[sourceIndex];
-        //         flippedImageData.data[targetIndex + 1] = maskImageData.data[sourceIndex + 1];
-        //         flippedImageData.data[targetIndex + 2] = maskImageData.data[sourceIndex + 2];
-        //         flippedImageData.data[targetIndex + 3] = maskImageData.data[sourceIndex + 3];
-        //     }
-        // }
         console.log('샘플 이미지', checkImageData)
+        const srgb = [0,0,0,0]
+        for (let i=0; i<307200; i++) {
+            if (checkImageData.data[4*i]!=0) {
+                srgb[0] ++;
+            }
+            if (checkImageData.data[4*i+1]!=0) {
+                srgb[1] ++;
+            }
+            if (checkImageData.data[4*i+2]!=0) {
+                srgb[2] ++;
+            }
+            if (checkImageData.data[4*i+3]!=0) {
+                srgb[3] ++;
+            }
+        }
+        console.log('srgb', srgb)
         console.log('마스크데이터', maskImageData)
         return (
             <div className="check-container">
@@ -115,11 +117,11 @@ class Check extends Component {
                             const ctx = canvasRef.getContext('2d');
 
                             // Canvas의 크기를 이미지 데이터 크기에 맞게 설정
-                            canvasRef.width = maskImageData.width;
-                            canvasRef.height = maskImageData.height;
+                            canvasRef.width = maskImageBitmap.width;
+                            canvasRef.height = maskImageBitmap.height;
 
                             // ImageBitmap을 ImageData로 변환
-                            imageBitmapToImageData(maskImageData).then(imageData => {
+                            imageBitmapToImageData(maskImageBitmap).then(imageData => {
                                 // 이미지 데이터를 캔버스에 그립니다
                                 ctx.putImageData(imageData, 0, 0);
                             });
@@ -134,106 +136,3 @@ class Check extends Component {
 }
 
 export default Check;
-
-
-// import React, { Component } from 'react';
-
-// class ImagePixelCheckComponent extends Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             imageLoaded: false,
-//             pixelCoordinates: [
-//                 { x: 100, y: 100 },
-//                 { x: 150, y: 200 },
-//                 // ... Add more pixel coordinates
-//             ],
-//             pixelResults: [],
-//         };
-
-//         this.imageRef = React.createRef();
-//     }
-
-//     componentDidMount() {
-//         this.loadImage();
-//     }
-
-//     loadImage() {
-//         const image = new Image();
-//         image.src = '/path/to/your/image.png'; // Replace with your image path
-
-//         image.onload = () => {
-//             this.imageRef.current.width = image.width;
-//             this.imageRef.current.height = image.height;
-
-//             this.setState({
-//                 imageLoaded: true,
-//             });
-
-//             this.checkPixelCoordinates(image);
-//         };
-//     }
-
-//     checkPixelCoordinates(image) {
-//         const { pixelCoordinates } = this.state;
-//         const canvas = document.createElement('canvas');
-//         const context = canvas.getContext('2d');
-
-//         canvas.width = image.width;
-//         canvas.height = image.height;
-
-//         context.drawImage(image, 0, 0, image.width, image.height);
-
-//         const pixelResults = pixelCoordinates.map((coord) => {
-//             const pixelData = context.getImageData(coord.x, coord.y, 1, 1).data;
-//             const isColoredPixel = pixelData[3] !== 0; // Check if alpha value is not 0
-
-//             return {
-//                 ...coord,
-//                 isColoredPixel,
-//             };
-//         });
-
-//         this.setState({
-//             pixelResults,
-//         });
-//     }
-
-//     render() {
-//         const { imageLoaded, pixelResults } = this.state;
-
-//         return (
-//             <div>
-//                 <h1>Image Pixel Check</h1>
-//                 {imageLoaded ? (
-//                     <div>
-//                         <div>
-//                             <img
-//                                 ref={this.imageRef}
-//                                 src="/path/to/your/image.png"
-//                                 alt="Target Image"
-//                             />
-//                         </div>
-//                         <div>
-//                             <h2>Pixel Results</h2>
-//                             <ul>
-//                                 {pixelResults.map((result, index) => (
-//                                     <li key={index}>
-//                                         {`Pixel (${result.x}, ${result.y}): ${
-//                                             result.isColoredPixel ? 'Colored' : 'Transparent'
-//                                         }`}
-//                                     </li>
-//                                 ))}
-//                             </ul>
-//                         </div>
-//                     </div>
-//                 ) : (
-//                     <p>Loading image...</p>
-//                 )}
-//             </div>
-//         );
-//     }
-// }
-
-// export default ImagePixelCheckComponent;
