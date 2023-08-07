4a9041ab0c08dc3c4f34dbd43f244a245be204fe
hzim <hzim0422@gmail.com>
2023-08-07 14:31:35 +0900

    [refactor]: 사용하지 않는 import 제거 230807 1430

--- back/B306/src/main/java/com/ssafy/B306/domain/openvidu/OpenViduController.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/openvidu/OpenViduController.java
@@ -1,6 +1,5 @@
 package com.ssafy.B306.domain.openvidu;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Value;
