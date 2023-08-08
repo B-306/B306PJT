c0fb0579ca34f9b4ccfdb1d78c35bf2e9cab3347
Junyoung Heo <junyoung449@gmail.com>
2023-08-08 17:36:17 +0900

    [fix] 오타 수정 230808 1736

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,13 +3,13 @@ module.exports = function(app){
     app.use(
         '/openvidu',
         createProxyMiddleware({
-          target: 'https://i9b306.q.ssafy.io:8443/',
+          target: 'https://i9b306.q.ssafy.io:8443',
           changeOrigin: true,
         })
       );app.use(
         '/api1',
         createProxyMiddleware({
-          target: 'https://i9b306.q.ssafy.io:8080/',
+          target: 'https://i9b306.q.ssafy.io:8080',
           changeOrigin: true,
         })
       );
