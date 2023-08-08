65d8b1faf1ff4110899f529f201e161814b2e87b
Junyoung Heo <junyoung449@gmail.com>
2023-08-08 17:18:19 +0900

    [fix] setupProxy.js 코드 신버전으로 수정 230808 1718

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,17 +2,15 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app){
     app.use(
         '/api',
-        proxy({
-            target: 'https://i9b306.q.ssafy.io:8443/',
-            changeOrigin:true,
+        createProxyMiddleware({
+          target: 'https://i9b306.q.ssafy.io:8443/',
+          changeOrigin: true,
         })
-    )
-    app.use(
+      );app.use(
         '/api1',
-        proxy({
-            target: 'https://i9b306.q.ssafy.io:8080/',
-            changeOrigin:true,
+        createProxyMiddleware({
+          target: 'https://i9b306.q.ssafy.io:8080/',
+          changeOrigin: true,
         })
-    )
-    
+      );
 }
