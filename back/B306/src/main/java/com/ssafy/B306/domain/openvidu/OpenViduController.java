ea49b061e00e8bc6982fdc5a420fe4d614f1d3be
fsddcx8624 <fsddcx8624@gmail.com>
2023-08-08 15:30:09 +0900

    [fix] openvidu connect 230808 1529 openvidu관련 로직 수정

--- back/B306/src/main/java/com/ssafy/B306/domain/openvidu/OpenViduController.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/openvidu/OpenViduController.java
@@ -1,34 +1,81 @@
 package com.ssafy.B306.domain.openvidu;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Value;
-import io.openvidu.java.client.*;
-import org.springframework.http.ResponseEntity;
+import javax.annotation.PostConstruct;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import io.openvidu.java.client.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class OpenViduController {
 
+	private final OpenViduService openViduService;
+
 	@Value("${openvidu.url}")
-	private String openViduUrl;
+	private String OPENVIDU_URL;
 
 	@Value("${openvidu.secret}")
-	private String openViduSecret;
+	private String OPENVIDU_SECRET;
 
-	@RequestMapping("/create-session")
-	public ResponseEntity<?> createSession() throws OpenViduHttpException {
-		OpenVidu openVidu = new OpenVidu(openViduUrl, openViduSecret);
+	private OpenVidu openvidu;
 
-		try {
-			Session session = openVidu.createSession();
-			String token = session.generateToken();
-
-			// 세션과 토큰 정보를 클라이언트에게 전달하거나 필요한 작업을 수행
-
-			return ResponseEntity.ok("Session created with token: " + token);
-		} catch (OpenViduJavaClientException e) {
-			return ResponseEntity.status(500).body("Error creating session: " + e.getMessage());
-		}
+	@PostConstruct
+	public void init() {
+		this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
 	}
+
+	/**
+	 * @param params The Session properties
+	 * @return The Session ID
+	 */
+	@PostMapping("/sessions")
+	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+			throws OpenViduJavaClientException, OpenViduHttpException {
+		SessionProperties properties = SessionProperties.fromJson(params).build();
+
+		Session session = openvidu.createSession(properties);
+		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+	}
+
+	/**
+	 * @param sessionId The Session in which to create the Connection
+	 * @param params    The Connection properties
+	 * @return The Token associated to the Connection
+	 */
+	@PostMapping("/sessions/{sessionId}/connections")
+	public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+												   @RequestBody(required = false) Map<String, Object> params)
+			throws OpenViduJavaClientException, OpenViduHttpException {
+		Session session = openvidu.getActiveSession(sessionId);
+		if (session == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+//		.fromJson(params)
+		ConnectionProperties properties = ConnectionProperties
+				.fromJson(params)
+//                .build();
+//                .Builder()
+				.type(ConnectionType.WEBRTC)
+//                .data(new Gson().toJson(params))
+				.role(OpenViduRole.PUBLISHER)
+				.kurentoOptions(
+						new KurentoOptions.Builder()
+								.allowedFilters(new String[]{"GStreamerFilter", "FaceOverlayFilter"})
+								.build())
+				.build();
+		Connection connection = session.createConnection(properties);
+		return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+	}
+
 }
