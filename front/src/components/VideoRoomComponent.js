7c1c431e1aee209824229371e13750c842060872
JNH <math_star@naver.com>
2023-08-09 13:33:58 +0900

    [fix]디버그 할거임 230809 1333

--- front/src/components/VideoRoomComponent.js
+++ front/src/components/VideoRoomComponent.js
@@ -11,7 +11,7 @@ import Check from './game/Check';
 import OpenViduLayout from '../layout/openvidu-layout';
 import UserModel from '../models/user-model';
 import ToolbarComponent from './toolbar/ToolbarComponent';
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
 
 
 var localUser = new UserModel();
@@ -21,7 +21,7 @@ const openvidu_key = process.env.REACT_APP_OPENVIDU_KEY;
 class VideoRoomComponent extends Component {
 
     constructor(props) {
-        let roomCode = v4();
+        // let roomCode = v4();
         super(props);
         this.hasBeenUpdated = false;
         this.layout = new OpenViduLayout();
