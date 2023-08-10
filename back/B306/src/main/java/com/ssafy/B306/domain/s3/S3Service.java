99c7fb24867293236b7a1e46b116e2a41939a942
fsddcx8624 <fsddcx8624@gmail.com>
2023-08-11 01:37:24 +0900

    [feat] add image sort -jpg- 230811 0137

--- back/B306/src/main/java/com/ssafy/B306/domain/s3/S3Service.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/s3/S3Service.java
@@ -30,6 +30,7 @@ public class S3Service {
 
         //content type을 지정해서 올려주지 않으면 자동으로 "application/octet-stream"으로 고정이 되서 링크 클릭시 웹에서 열리는게 아니라 자동 다운이 시작됨.
         switch (ext) {
+            case "jpg":
             case "jpeg":
                 contentType = "image/jpeg";
                 break;
@@ -47,6 +48,7 @@ public class S3Service {
         try {
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(contentType);
+            metadata.setContentLength(file.getSize());
             //multipartfile을 putObject를 통해 S3에 업로드
             //bucket 버킷 이름, fileName 파일 이름, multipartFile.getInputStream() - multipartFile을 InputStream형태로 변환
             amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
