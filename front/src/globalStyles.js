9a83ccc6ebe5759bfae608d2b5dab4f18358fb5a
주영인 <jyi8714@naver.com>
2023-08-11 17:37:10 +0900

    [Style] AuthForm 스타일 변경

--- front/src/globalStyles.js
+++ front/src/globalStyles.js
@@ -7,12 +7,12 @@ const GlobalStyles = createGlobalStyle`
   body {
     background-image: url(${backgroundImage}), url(${backgroundImage});
     // background-size: cover;
-    background-size: 40% auto, 40% auto;
+    background-size: 30% auto, 30% auto;
     // background-size: contain;
     background-repeat: no-repeat, no-repeat;
     background-attachment: fixed;
-    // background-position: left, right;
-    background-position: left top, right top;
+    background-position: left, right;
+    // background-position: left top, right top;
     /* 추가적인 스타일링을 원하는 경우 여기에 작성 */
     margin: 0; /* body의 기본 margin을 제거 */
     font-family: Ftstardust;
