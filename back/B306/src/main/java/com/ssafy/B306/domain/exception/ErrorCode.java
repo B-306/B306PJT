f3c4887a769b150b94e5b70b4000bc6a40ff4216
송혁준 <thdgurws98@naver.com>
2023-08-13 22:51:02 +0900

    [feat] custom exception access, refresh token & user 관련 추가 230813 2250

--- back/B306/src/main/java/com/ssafy/B306/domain/exception/ErrorCode.java
+++ back/B306/src/main/java/com/ssafy/B306/domain/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USEREMAIL_DUPLICATED(HttpStatus.CONFLICT, "");
+    ACCESSTOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "Access Token이 만료되었습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "Refresh Token이 만료되었습니다."),
+
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 유저가 없습니다."),
+    USEREMAIL_DUPLICATED(HttpStatus.CONFLICT, "이미 가입된 이메일입니다.");
 
     private HttpStatus httpStatus;
     private String message;
