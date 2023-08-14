4ac2cffc351fd1b8614baa9ec489827990e7e703
JNH <math_star@naver.com>
2023-08-14 13:25:01 +0900

    [Fix] 사진 데이터 CORS 에러 수정 230814 1324

--- front/src/components/game/Check.js
+++ front/src/components/game/Check.js
@@ -40,7 +40,7 @@ async function imageBitmapToImageData(imageBitmap) {
 function loadImageAndProcess(templateURL) {
     return new Promise(async (resolve) => {
         const checkImage = new Image();
-        checkImage.crossOrigin = "Anonymous";
+        checkImage.crossOrigin = "anonymous";
         checkImage.src = templateURL;
         await checkImage.decode();
         const canvas = document.createElement('canvas');
