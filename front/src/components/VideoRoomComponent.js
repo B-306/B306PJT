fcf0b0626504860468aefd901248ccc7ca62ddf8
JNH <math_star@naver.com>
2023-08-16 14:57:26 +0900

    [Design] sub streaming 화면 배치 조정 20230816 1457

--- front/src/components/VideoRoomComponent.js
+++ front/src/components/VideoRoomComponent.js
@@ -756,23 +756,22 @@ class VideoRoomComponent extends Component {
                             </p>
                         </Card>
                         )}
-                        <div className="scroll-container" style={{ overflow: 'auto', maxheight: '70vh' }} >
                         {!showCounter && this.state.subscribers.map((sub, i) => (
                             // <div key={i} className="OT_root OT_publisher custom-class" id="remoteUsers" style={{
-                            <div key={i} className="stream-item" id="remoteUsers" style={{ 
+                            <div key={i} id="remoteUsers" style={{ 
                                 display:'inline-block',
                                 width:'12%',
                                 height:'15%',
-                                // position:'relative',
+                                position:'relative',
                                 margin: '0px 2px 0px', // 스트림 간격 조절
                                 transform: `translate(-50%, -50%) translateX(${20 * i}%)`, // i에 따라서 x 방향으로 이동
-                                // top: '75px',
-                                // left: '100px',
+                                top: '75px',
+                                left: '100px',
                                 }}>
                                     <StreamComponent user={sub} streamId={sub.streamManager.stream.streamId} />
                             </div>
                     ))}
-                    </div>
+                    {/* </div> */}
                     {localUser !== undefined && localUser.getStreamManager() !== undefined && (
                         // 화면 위치 및 크기 조정
                         <div id="localUser" style={{ display:'inline-block', width:'720px', height:'540px', top:'60%', transform: 'translate(-50%, -50%)', left:'35%', position:'absolute'}}>
